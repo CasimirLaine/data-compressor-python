233aba2f85b998cc605e0ccf879da695e0476307
Casimir Laine <casimir@choicely.com>
2022-07-29 00:54:48 +0300

    Added cleanup of old modules to the setup script.

--- setup.py
+++ setup.py
@@ -2,9 +2,25 @@ import os
 
 
 def install_requirements():
-    stream = os.popen('python -m pip install -r requirements.txt')
-    output = stream.read()
-    print(output)
+    req_file = 'requirements.txt'
+    del_req_file = '.del_requirements.txt'
+    commands = [
+        f'python -m pip install --upgrade pip',
+        f'python -m pip freeze > {del_req_file}',
+        f'python -m pip uninstall -y -r {del_req_file}',
+        f'python -m pip install -r {req_file}',
+    ]
+
+    def del_frozen_requirements():
+        if os.path.isfile(del_req_file):
+            os.remove(del_req_file)
+
+    del_frozen_requirements()
+    for command in commands:
+        stream = os.popen(command)
+        output = stream.read()
+        print(output)
+    del_frozen_requirements()
 
 
 if __name__ == '__main__':
