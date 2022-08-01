eb6aa8843ebbdb2f0407fbd5174db394f7c74d3c
Casimir Laine <casimir@choicely.com>
2022-08-02 01:32:46 +0300

    Fixed docstring locations.

--- src/lz/__init__.py
+++ src/lz/__init__.py
@@ -1,12 +1,15 @@
+"""
+This module is used as an API for developers to encode and decode data with LZ77.
+"""
 _STRING_ENCODING = 'UTF-8'
-
-"""
-Compressor that can can be modified in constructor arguments.
-Functions as the API for compression process.
-"""
 
 
 class LZCompressor:
+    """
+    Compressor that can can be modified in constructor arguments.
+    Functions as the API for compression process.
+    """
+
     def __init__(
             self,
             *,
@@ -17,22 +20,19 @@ class LZCompressor:
         self.search_buffer_size = search_buffer_size
         self.lookahead_buffer_size = lookahead_buffer_size
 
-    """
-    Function to call to provide input for the compressor to compress.
-    Returns the compressed text.
-    """
-
     def encode(self, data: str):
+        """
+        Function to call to provide input for the compressor to compress.
+        Returns the compressed text.
+        """
         encoding_process = _EncodingProcess(self, data)
         return encoding_process.encode()
 
 
-"""
-Protected class to maintain the internal state of a single compression run.
-"""
-
-
 class _EncodingProcess:
+    """
+    Protected class to maintain the internal state of a single compression run.
+    """
 
     def __init__(self, compressor: LZCompressor, data: str):
         super().__init__()
@@ -40,11 +40,10 @@ class _EncodingProcess:
         self._original_data = data.encode(encoding=_STRING_ENCODING)
         self._cursor = -1
 
-    """
-    Used to start the encoding process internally.
-    """
-
     def encode(self) -> str:
+        """
+        Used to start the encoding process internally.
+        """
         encoded_bytearray = bytearray()
         index = 0
         while True:
@@ -61,16 +60,15 @@ class _EncodingProcess:
                 index += 1
         return encoded_bytearray.decode(encoding=_STRING_ENCODING)
 
-    """
-    Returns the smallest index of the search buffer.
-    """
-
     def search_limit(self):
+        """
+        Returns the smallest index of the search buffer.
+        """
+
         return max(self._cursor - self._compressor.search_buffer_size, 0)
 
-    """
-    Returns the largest index of the lookahead buffer.
-    """
-
     def lookahead_limit(self):
+        """
+        Returns the largest index of the lookahead buffer.
+        """
         return min(self._cursor + self._compressor.lookahead_buffer_size, len(self._original_data))
