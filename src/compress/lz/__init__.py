02a9df7228828088efd3cf5fc302660e20bb21e6
Casimir Laine <casimir@choicely.com>
2022-08-05 21:41:56 +0300

    Cleaned up the compression algorithm.

--- src/compress/lz/__init__.py
+++ src/compress/lz/__init__.py
@@ -1,7 +1,10 @@
 """
 This module is used as an API for developers to encode and decode data with the Lempel-Ziv algorithm.
 """
+import sys
 from typing import Optional, Type
+
+from bitarray import bitarray
 
 from compress.common import CompressionAlgorithm, Decoder, Encoder
 
@@ -14,11 +17,12 @@ class LZEncode(Encoder):
     Functions as the API for compression process.
     Uses the Lempel-Ziv encoding algorithm to compress data.
     """
+
     def __init__(
             self,
             *,
             search_buffer_size: int = 255,
-            lookahead_buffer_size: int = 255,
+            lookahead_buffer_size: int = 128,
     ):
         super().__init__()
         if lookahead_buffer_size > search_buffer_size:
@@ -68,6 +72,34 @@ class _EncodingProcess:
         self._cursor = -1
         self._matches: dict[str, list[int]] = {}
 
+    def _find_longest_match(self) -> tuple[int, int]:
+        character = self.original_data[self._cursor]
+        match_indices = self.get_match(character)
+        if match_indices is None or match_indices[-1] < self.search_limit():
+            self.set_match(character, self._cursor)
+            return 0, 0
+        longest_match_offset = 0
+        longest_match_length = 0
+        for match_index in match_indices:
+            if match_index < self.search_limit():
+                continue
+            left_offset = self._cursor - match_index
+            for lookahead_pointer in range(
+                    min(self._cursor + 1, self.data_length - 1),
+                    self.lookahead_limit() + 1
+            ):
+                next_char_offset = lookahead_pointer - self._cursor
+                if match_index + next_char_offset >= self._cursor:
+                    break
+                if self.original_data[lookahead_pointer] != self.original_data[match_index + next_char_offset]:
+                    break
+                match_length = lookahead_pointer - self._cursor
+                if longest_match_length <= match_length:
+                    longest_match_offset = left_offset
+                    longest_match_length = match_length
+        self.set_match(character, self._cursor)
+        return longest_match_offset, longest_match_length
+
     def encode(self) -> bytes:
         """
         Used to start the encoding process internally.
@@ -75,41 +107,14 @@ class _EncodingProcess:
         encoded_buffer = bytearray()
         self._cursor = 0
         while self._cursor < self.data_length:
-            match_buffer = self.original_data[self._cursor:self._cursor + 1]
-            match_indices = self.get_match(match_buffer)
-            self.set_match(match_buffer, self._cursor)
-            left_offset = 0
-            match_length = 0
-            if match_indices is not None and match_indices[-1] >= self.search_limit():
-                longest_match_offset = self._cursor - match_indices[-1]
-                longest_match = match_buffer
-                for match_index in match_indices:
-                    if match_index < self.search_limit():
-                        continue
-                    match_buffer = self.original_data[self._cursor:self._cursor + 1]
-                    for lookahead_pointer in range(
-                            min(self._cursor + 1, self.data_length - 1),
-                            self.lookahead_limit() + 1
-                    ):
-                        next_char_index = lookahead_pointer - self._cursor
-                        if match_index + next_char_index >= self._cursor:
-                            break
-                        if self.original_data[lookahead_pointer] != self.original_data[match_index + next_char_index]:
-                            break
-                        left_offset = self._cursor - match_index
-                        match_buffer += self.original_data[lookahead_pointer: lookahead_pointer + 1]
-                        if len(longest_match) <= len(match_buffer):
-                            longest_match = match_buffer
-                            longest_match_offset = left_offset
-                match_length = len(longest_match)
-                left_offset = longest_match_offset
-            encoded_buffer.extend(match_length.to_bytes(length=1, byteorder='big', signed=False))
+            left_offset, match_length = self._find_longest_match()
+            encoded_buffer.extend(match_length.to_bytes(length=1, byteorder=sys.byteorder, signed=False))
             if match_length == 0:
                 encoded_buffer.extend(
-                    self.original_data[self._cursor].to_bytes(length=1, byteorder='big', signed=False))
+                    self.original_data[self._cursor].to_bytes(length=1, byteorder=sys.byteorder, signed=False))
                 self._cursor += 1
             else:
-                encoded_buffer.extend(left_offset.to_bytes(length=1, byteorder='big', signed=False))
+                encoded_buffer.extend(left_offset.to_bytes(length=1, byteorder=sys.byteorder, signed=False))
                 self._cursor += match_length
         return encoded_buffer
 
