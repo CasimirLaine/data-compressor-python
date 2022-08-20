b4f26e1123773f5388828a6aa0864438d6b92f38
Casimir Laine <casimir@choicely.com>
2022-08-20 15:14:49 +0300

    Added some testing for files.

--- tests/test_compress/__init__.py
+++ tests/test_compress/__init__.py
@@ -2,6 +2,6 @@ import os
 import sys
 from pathlib import Path
 
-root_path = Path(__file__).resolve().parent.parent.parent
-path_prefix = f'{root_path}{os.sep}'
+ROOT_PATH = Path(__file__).resolve().parent.parent.parent
+path_prefix = f'{ROOT_PATH}{os.sep}'
 sys.path.append(f'{path_prefix}src')
--- tests/test_compress/test_algo.py
+++ tests/test_compress/test_algo.py
@@ -1,15 +1,24 @@
+import os
 import random
 import string
 
 import pytest
 
 from compress import huffman, lz
+from compress.common import io
+from test_compress import ROOT_PATH
 
-__N = [1_000, 10_000, 100_000, 1_000_000]
+__N = [10_000, 100_000, 1_000_000, 10_000_000]
 
 __ALGORITHMS = [
-    (huffman.HuffmanEncoder, huffman.HuffmanDecoder),
-    # (lz.LZEncoder, lz.LZDecoder)
+    huffman.Huffman,
+    # lz.LZ,
+]
+
+__FILES = [
+    'sample/lorem.txt',
+    'sample/image.jpeg',
+    'sample/image.png',
 ]
 
 
@@ -17,7 +26,15 @@ def _gen_combinations():
     combinations = []
     for a in __ALGORITHMS:
         for n in __N:
-            combinations.append((*a, n))
+            combinations.append((a, n))
+    return combinations
+
+
+def _gen_file_combinations():
+    combinations = []
+    for a in __ALGORITHMS:
+        for f in __FILES:
+            combinations.append((a, f))
     return combinations
 
 
@@ -25,16 +42,27 @@ def _random_bytes(n):
     return ''.join(random.choice(string.printable) for _ in range(n)).encode()
 
 
-@pytest.mark.parametrize("encoder, decoder, n", _gen_combinations())
-def test_encode_smaller(encoder, decoder, n):
+@pytest.mark.parametrize("algorithm, n", _gen_combinations())
+def test_encode_smaller(algorithm, n):
+    encoder = algorithm.get_encoder()
     input_bytes = _random_bytes(n)
     compressor = encoder()
     result = compressor.encode(input_bytes)
     assert len(result) <= len(input_bytes)
 
 
-@pytest.mark.parametrize("encoder, decoder, n", _gen_combinations())
-def test_decode_same_length(encoder, decoder, n):
+@pytest.mark.parametrize("algorithm, n", _gen_combinations())
+def test_encode_smaller_enough(algorithm, n):
+    encoder = algorithm.get_encoder()
+    input_bytes = _random_bytes(n)
+    compressor = encoder()
+    result = compressor.encode(input_bytes)
+    assert len(result) <= len(input_bytes) * 0.9
+
+
+@pytest.mark.parametrize("algorithm, n", _gen_combinations())
+def test_decode_same_length(algorithm, n):
+    encoder, decoder = (algorithm.get_encoder(), algorithm.get_decoder())
     input_bytes = _random_bytes(n)
     compressor = encoder()
     result = compressor.encode(input_bytes)
@@ -43,11 +71,28 @@ def test_decode_same_length(encoder, decoder, n):
     assert len(input_bytes) == len(decoded_bytes)
 
 
-@pytest.mark.parametrize("encoder, decoder, n", _gen_combinations())
-def test_decode(encoder, decoder, n):
+@pytest.mark.parametrize("algorithm, n", _gen_combinations())
+def test_decode(algorithm, n):
+    encoder, decoder = (algorithm.get_encoder(), algorithm.get_decoder())
     input_bytes = _random_bytes(n)
     compressor = encoder()
     result = compressor.encode(input_bytes)
+    decompressor = decoder()
+    decoded_bytes = decompressor.decode(result)
+    assert input_bytes == decoded_bytes
+
+
+@pytest.mark.parametrize("algorithm, file", _gen_file_combinations())
+def test_read_file(algorithm, file):
+    file = os.path.join(ROOT_PATH, file)
+    encoder, decoder = (algorithm.get_encoder(), algorithm.get_decoder())
+    input_bytes = io.read_file(file)
+    compressor = encoder()
+    result = compressor.encode(input_bytes)
+    # output_path = f'{file}.output'
+    # io.write_file(output_path, result)
+    # compressed_bytes = io.read_file(file)
+    # os.remove(output_path)
     decompressor = decoder()
     decoded_bytes = decompressor.decode(result)
     assert input_bytes == decoded_bytes
